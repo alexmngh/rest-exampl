a3c4189181a18243995162c417460aabcceff7a1
alexmngh <alexmnsbs@gmail.com>
2020-04-16 19:01:49 +0300

    Старт

---
+++ src/main/java/com/java/lectric/rest_exam/RestExamApplication.java
@@ -0,0 +1,13 @@
+package com.java.lectric.rest_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestExamApplication {
+    /** Запуск приложения  */
+    public static void main(String[] args) {
+        SpringApplication.run(RestExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/java/lectric/rest_exam/RestExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.lectric.rest_exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
